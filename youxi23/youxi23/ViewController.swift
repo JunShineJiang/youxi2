3ac16dfffd41b1c45ff19fb2ed983809dc1652fa
angryCoder <791100345@qq.com>
2016-12-09 15:57:04 +0800

    是初始化项目

---
+++ youxi23/youxi23/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youxi23
+//
+//  Created by 江君祥 on 2016/12/9.
+//  Copyright © 2016年 江君祥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
