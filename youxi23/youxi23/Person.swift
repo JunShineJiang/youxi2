9dabdedd70e14be8d51622e661a7ff089364dbfe
angryCoder <791100345@qq.com>
2016-12-09 16:00:10 +0800

    添加了Person类

---
+++ youxi23/youxi23/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  youxi23
+//
+//  Created by 江君祥 on 2016/12/9.
+//  Copyright © 2016年 江君祥. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String?
+}
